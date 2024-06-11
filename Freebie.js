769b9a56343a8df4a1b478c625276df9648d17b2
Devan-Sunshine <37220936+dskw1@users.noreply.github.com>
2024-06-10 21:19:39 -0600

    Update Freebie.js

--- Freebie.js
+++ Freebie.js
@@ -1,5 +1,11 @@
 let total = 0;
 
+/**
+ *Adds the given number to the total and allows chaining.
+ * @param {number} [n=1] - The number to add to the total. Defaults to 1 if not provided.
+ * @returns {function} - Returns a function that can be called with another number to continue the chain.
+ */
+
 function add(n = 1) {
     total += n;
     return (m) => add(m);
