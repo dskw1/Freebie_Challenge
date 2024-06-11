edfbaaf525f310e432f2af41fb197e657845461b
Devan-Sunshine <37220936+dskw1@users.noreply.github.com>
2024-06-10 21:44:01 -0600

    Create class.js

---
+++ class.js
@@ -0,0 +1,19 @@
+class Todo {
+    constructor(name, date, complete) {
+        this.name = new String(name);
+      
+      if (!(date instanceof Date) || isNaN(date)) {
+            throw new Error('Invalid date');
+        }
+        this.date = date;
+
+        this.complete = new Boolean(complete);
+        this._description = undefined;
+        this._newString = undefined;
+    }
+  
+static markAs(todo, status) {
+        if (todo instanceof Todo) {
+            todo.complete = new Boolean(status);
+        }
+      }
